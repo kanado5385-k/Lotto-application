2af6e32adeea698718390dc11546c8b7c7d728ed
이윤빈 <kanado20000@naver.com>
2024-12-05 14:19:47 +0900

    :sparkles: feat: 당첨 순위에 대한 당첨금액을 관리하는 enum 구현 완료

---
+++ src/main/java/lotto/enums/WinningPrice.java
@@ -0,0 +1,28 @@
+package lotto.enums;
+
+public enum WinningPrice {
+    FIRST_PLACE(2000000000),
+    SECOND_PLACE(30000000),
+    THIRD_PLACE(1500000),
+    FOURTH_PLACE(50000),
+    FIFTH_PLACE(5000);
+
+    private final int price;
+
+    WinningPrice(int price) {
+        this.price = price;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public static WinningPrice fromPrice(int price) {
+        for (WinningPrice type : WinningPrice.values()) {
+            if (type.price == price) {
+                return type;
+            }
+        }
+        throw new IllegalArgumentException("No matching WinningPrice for price: " + price);
+    }
+}
