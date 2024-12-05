7806c21006fc0fc300bbafbfbc297c088cc8a6b8
이윤빈 <kanado20000@naver.com>
2024-12-05 13:40:32 +0900

    :sparkles: feat: 당첨번호들의 유효성 검사하는 클래스 구현 완료

--- src/main/java/lotto/enums/messages/ErrorMessage.java
+++ src/main/java/lotto/enums/messages/ErrorMessage.java
@@ -8,12 +8,11 @@ public enum ErrorMessage implements SystemMessage {
         + "아닙니다. 다시 입력해 주세요."),
     INVALID_INT_NUMBER("숫자가 아닌 것이 입력 되었습니다. 다시 시도해주세요."),
     INVALID_INPUT_MONEY("입력하신 금액이 0원 이상이여야합니다. 다시 시도해주세요."),
-
     INVALID_LOTTO_QUANTITY("로또 번호는 6개여야 합니다. 다시 시도 해보겠습니다."),  
     NOT_NATURAL_NUMBER("자연수가 아닌 숫자입니다. 다시 시도해주세요."),
-    
-    PROBLEM_WITH_FILE("파일을 읽는 중 문제가 발생했습니다. 파일 경로와 파일 상태를 확인한 후 다시 시도해 주세요."),
-    LACK_OF_PRODUCT("재고 수량을 초과하여 구매할 수 없습니다. 다시 입력해 주세요.");
+    INVALID_WINNING_NUMBERS_QUANTITY("입력하신 당첨 번호 수량이 6개가 아닙니다. 다시 시도해주세요."),
+    INVALID_WINNING_NUMBER("입력하신 당첨 번호 중에 1~45 범위를 벗어난 숫자가 있습니다. 다시 시도해주세요."),
+    DUPLICATE_WINNING_NUMBER("입력하신 당첨 번호 중에 중복이 있습니다. 다시 시도해주세요.");
 
     private static final String ERROR_LOG_LEVEL = "[ERROR] ";
     private final String message;
---
+++ src/main/java/lotto/validator/WinningNumbersValidator.java
@@ -0,0 +1,29 @@
+package lotto.validator;
+
+import java.util.HashSet;
+import java.util.List;
+
+import lotto.enums.messages.ErrorMessage;
+
+public class WinningNumbersValidator {
+    private static final int QUANTITY_OF_WINNING_NUMBERS = 6;
+
+    public static void validateWinningNumbers(List<Integer> winningNumbers) {
+        if (winningNumbers.size() != QUANTITY_OF_WINNING_NUMBERS) {
+            throw new IllegalArgumentException(
+                ErrorMessage.INVALID_WINNING_NUMBERS_QUANTITY.getMessage());
+        }
+        for (Integer number : winningNumbers) {
+            if ((number < 1) || (number > 45)) {
+                throw new IllegalArgumentException(ErrorMessage.INVALID_WINNING_NUMBER.getMessage());
+            }
+        }
+
+        HashSet<Integer> uniqueNumbers = new HashSet<>();
+        for (Integer number : winningNumbers) {
+            if (!uniqueNumbers.add(number)) {
+                throw new IllegalArgumentException(ErrorMessage.DUPLICATE_WINNING_NUMBER.getMessage());
+            }
+        }
+    }
+}
