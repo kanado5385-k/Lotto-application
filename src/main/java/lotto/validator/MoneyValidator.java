38f9eeee1dc1c0df1b06f642ca6cefe140ec6134
이윤빈 <kanado20000@naver.com>
2024-12-05 12:33:42 +0900

    :recycle: refactor: 입력 금액 유효성 검사 함수 함수 수정

--- src/main/java/lotto/validator/MoneyValidator.java
+++ src/main/java/lotto/validator/MoneyValidator.java
@@ -5,7 +5,6 @@ import lotto.enums.messages.ErrorMessage;
 public class MoneyValidator {
     
     public static void validateMoney(int money) {
-        int lottoPrice = 1000;
         if (money % 1000 != 0) {
             throw new IllegalArgumentException(ErrorMessage.INVALID_INPUT_MONEY.getMessage());
         }
