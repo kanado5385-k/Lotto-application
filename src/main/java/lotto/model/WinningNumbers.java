6535bd26358b8441eed7bb71041fc1b52837c95a
이윤빈 <kanado20000@naver.com>
2024-12-05 13:30:22 +0900

    :sparkles: feat: 당첨번호와 보너스 번호를 관리하는 WinningNumbers 클래스 추가

---
+++ src/main/java/lotto/model/WinningNumbers.java
@@ -0,0 +1,13 @@
+package lotto.model;
+
+import java.util.List;
+
+public class WinningNumbers {
+    private final List<Integer> winningNumbers;
+    private final Integer bonusNumber;
+
+    public WinningNumbers(String winningNumbers, int bonusNumber) {
+        
+    }
+
+}
