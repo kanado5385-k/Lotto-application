4a3a4770f866f8d98ef65b9c656ca87b0a36cba6
이윤빈 <kanado20000@naver.com>
2024-12-05 12:16:37 +0900

    :sparkles: feat: 예외발생시 error 메세지 출력을 위한 ViewOutput 클래스 구현 완료

---
+++ src/main/java/lotto/view/ExceptionOutputView.java
@@ -0,0 +1,8 @@
+package lotto.view;
+
+public class ExceptionOutputView {
+    
+    public static void printErrorMessage(String errorMessage){
+        System.out.println(errorMessage + System.lineSeparator());
+    }
+}
