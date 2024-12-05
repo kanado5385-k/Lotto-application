e1ce5428f96561f7e1b9f0121282bcf4417c53a7
이윤빈 <kanado20000@naver.com>
2024-12-05 14:03:28 +0900

    :sparkles: feat: WinningNumbers 클래스의 생성자 구현 완료

--- src/main/java/lotto/model/WinningNumbers.java
+++ src/main/java/lotto/model/WinningNumbers.java
@@ -3,14 +3,23 @@ package lotto.model;
 import java.util.List;
 
 import lotto.utilities.Parser;
+import lotto.utilities.Splitter;
+import lotto.validator.WinningNumbersValidator;
 
 public class WinningNumbers {
     private final List<Integer> winningNumbers;
     private final Integer bonusNumber;
 
-    public WinningNumbers(String winningNumbers, int bonusNumber) {
-        
-        List<Integer> intWinningNumbers = Parser.parseNumbersToInt(null)
+    public WinningNumbers(String winningNumbers, String bonusNumber) {
+        List<String> splittedWinningNumbers = Splitter.splitNumbers(winningNumbers);
+        List<Integer> intWinningNumbers = Parser.parseNumbersToInt(splittedWinningNumbers);
+        WinningNumbersValidator.validateWinningNumbers(intWinningNumbers);
+
+        int intBonusNumber = Parser.parseNumberToInt(bonusNumber);
+        WinningNumbersValidator.validateBonusNumber(intBonusNumber, intWinningNumbers);
+
+        this.winningNumbers = intWinningNumbers;
+        this.bonusNumber = intBonusNumber;
     }
 
 }
